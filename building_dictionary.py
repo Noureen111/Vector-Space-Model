55983e28ada70dc3b839ab1da15d292980b82e8a
Noureen <noureeng334@gmail.com>
2021-11-12 02:08:53 +0500

    IR

---
+++ building_dictionary.py
@@ -0,0 +1,12 @@
+### IMPORTING LIBRARIES
+
+import nltk
+import csv
+import math
+from sklearn.metrics.pairwise import cosine_similarity
+from collections import OrderedDict
+import numpy as np
+import operator
+from processing import Preprocessing
+
+
